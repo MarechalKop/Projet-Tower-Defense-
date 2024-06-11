7c9fa670f4912af4bdde6aaa96efedf6d70add55
Sousada <sousada.boualapha@outlook.fr>
2024-06-11 16:01:20 +0200

    tours qui peuvent tirer sur les ennemis en mvt
    i'm in the ghetto ratatata

--- src/jeu.cpp
+++ src/jeu.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 #include "tours.hpp"
 #include "ennemis.hpp"
 #include "jeu.hpp"
+#include "graphe.hpp"
 
 namespace Jeu {
     int total_argent {0};
 }
 
 void tourAttaqueEnnemi(Tour& tour, Ennemi& ennemi) {
-    ennemi.degatsEnnemi(tour.puissance);
-    if (ennemi.estMort()) {
-        Jeu::total_argent += ennemi.recompense;
+    if (tour.peutTirer() && tour.estDansPortee(ennemi.positionActuelle.x, ennemi.positionActuelle.y)) {
+        ennemi.degatsEnnemi(tour.puissance);
+        tour.miseAJourTempsTir();
+
+        if (ennemi.estMort()) {
+            Jeu::total_argent += ennemi.recompense;
+        }
+    }
+}
+
+void mettreAJourJeu(std::vector<Tour>& tours, std::vector<Ennemi>& ennemis, float dt) {
+    // Mettre à jour les positions des ennemis
+    for (auto& ennemi : ennemis) {
+        ennemi.avancer(dt);
+    }
+
+    // Faire tirer les tours sur les ennemis à portée
+    for (auto& tour : tours) {
+        for (auto& ennemi : ennemis) {
+            if (!ennemi.estMort()) {
+                tourAttaqueEnnemi(tour, ennemi);
+            }
+        }
     }
 }
--- src/jeu.hpp
+++ src/jeu.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include <vector>
 #include "tours.hpp"
 #include "ennemis.hpp"
 
 void tourAttaqueEnnemi(Tour& tour, Ennemi& ennemi);
+void mettreAJourJeu(std::vector<Tour>& tours, std::vector<Ennemi>& ennemis, float dt);
